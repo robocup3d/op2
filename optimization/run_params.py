5525f4e0088ca26c8f1b278a56916796cdd8c314
robocup3d <robocup3d@163.com>
2019-06-19 21:49:05 +0800

    update ignore

--- optimization/run_params.py
+++ optimization/run_params.py
@@ -7,15 +7,13 @@ ROBOT_TYPE = 2
 
 
 def write2file(param_list, file_path):
-    with open(file_path, 'r+') as file:
-        param_lines = file.readlines()
-        param_names = [i.split('\t')[0] for i in param_lines]
-
+    _, param_names = read_params()
+    assert len(param_list) == len(param_names)
     new_lines = [
         e + '\t' + str(param_list[i]) + '\n'
         for i, e in enumerate(param_names)
     ]
-    with open(file_path, 'w+') as file:
+    with open(file_path, 'w') as file:
         file.writelines(new_lines)
 
 
@@ -26,8 +24,6 @@ def run(param_list, robot_type, abs_path='../paramfiles/optimizing.txt'):
     with open('walkout') as file:
         lines = file.readlines()[-1]
         try:
-
-            # time_cost = [float(_.strip('\n')) for _ in lines]
             return float(lines.strip('\n'))
         except ValueError:
             return 1000
@@ -39,7 +35,7 @@ def read_params(fn='../paramfiles/original.txt'):
         lines = [
             _ for
             _ in lines
-            if _[0] not in '#/'
+            if _[0] not in '#/ \t\n'
         ]
         params = [float(_) for _ in [i[:-1].split('\t')[-1] for i in lines]]
         params_name = [i[:-1].split('\t')[0] for i in lines]
@@ -79,7 +75,8 @@ def main():
     abs_path = os.path.abspath('..') + '/paramfiles/original.txt'
     # write2file(paramList, absPath)
     rst = run(param_list, ROBOT_TYPE, abs_path)
-    print('go to target average time cost:', rst)
+    # PLUS 85 DUE TO REWARD, HERE RECOVER TO ORIGINAL TIME
+    print('running average time cost:', rst+85)
 
 
 if __name__ == '__main__':
